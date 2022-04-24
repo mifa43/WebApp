6fda72ab2d00256cc857e4284773547dd8621893
Mifa43 <mifa43kotez@gmail.com>
2022-04-24 21:46:32 +0200

    dodat je main crud file u kome se pozivaju sql upiti

---
+++ userservice/src/crud.py
@@ -0,0 +1,24 @@
+from click import echo
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy import create_engine, Integer, String, ForeignKey, Column
+from sqlalchemy.ext.declarative import declarative_base
+from tableModel import User
+
+class Postgres():
+    def __init__(self):
+        self.engine = engine = create_engine(
+            "postgresql+psycopg2://mifa43:koliko43@user-service-postgres-dev/userservice",
+            isolation_level="SERIALIZABLE", echo=True
+        )
+        self.db = db = scoped_session(sessionmaker(bind=self.engine))
+        self.base = Base = declarative_base()
+
+    def createTable(self):
+        table1 = User().registerTable()
+        
+        return {"func": table1}
+
+    def insertUser(self):
+        data = User().insert()
+        return {"insert": data}
