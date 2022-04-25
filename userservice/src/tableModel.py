02443cce3a97d53e181b02b4b19e27bb9b95c764
Mifa43 <mifa43kotez@gmail.com>
2022-04-25 21:43:20 +0200

    izmene u User klasi, kreiranje tabele

--- userservice/src/tableModel.py
+++ userservice/src/tableModel.py
@@ -3,12 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
-engine = create_engine(
-                    "postgresql+psycopg2://mifa43:koliko43@user-service-postgres-dev/userservice",
-                    isolation_level="SERIALIZABLE", echo=True
-                )
+
 class User(Base):
     """Model tabele"""
+    
     __tablename__= "registration"   #ime tabele
      
     #kolone u tabeli i njihove karakteristike
@@ -16,34 +14,5 @@ class User(Base):
     name = Column("username", String)
     lastName = Column("lastname",String)
     mail = Column("mail",String)
-    phoneNumber = Column("phonenumber", Integer)
+    phoneNumber = Column("phonenumber", String)
     password = Column("password",String)
-
-    def registerTable(self):
-
-        """Fukcija uzima model i genersi sql upit i kreira tabelu"""
-
-        Base.metadata.create_all(bind=engine)
-
-        return {"TableName": "Table created"}
-
-    def insert(self, name, lastName, mail, phoneNumber, password):
-        """Upisivanje korisnika u bazu 
-        Koristi User klasu, poziva atribute i dodeljuje vrednosti
-        """
-        Seassion = sessionmaker(bind=engine)
-        seassion = Seassion()
-
-        user = User()
-        user.name = f"{name}"
-        user.lastName = f"{lastName}"
-        user.mail = f"{mail}"
-        user.phoneNumber = int(phoneNumber)
-        user.password = f"{password}"
-
-
-        seassion.add(user)
-        seassion.commit()
-        print("user inserted")
-        seassion.close()
-        return {"TableName": "User inserted"}
