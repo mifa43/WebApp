ba03d9201bf42750604f16b0afd0a924489c6563
Mifa43 <mifa43kotez@gmail.com>
2022-04-25 23:02:33 +0200

    dodat komentar u modelu

--- userservice/src/models.py
+++ userservice/src/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+# model za hvatanje requesta i slanje u insert funkciju
+
 class UserModel(BaseModel):
     UserName: str
     UserLastName: str
