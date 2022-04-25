fa466131bb6950bfd77eb75ee937dbcd3ace619d
Mifa43 <mifa43kotez@gmail.com>
2022-04-25 22:56:40 +0200

    dodat komentar pojasnjenja

--- api/src/models.py
+++ api/src/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+# model za fastapi post metodu koja hvata requestove
+
 class RegisterForm(BaseModel):
     UserName: str
     UserLastName: str
