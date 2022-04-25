b1c6b48f8d48226da3305fc602a7b3c6ff28757c
Mifa43 <mifa43kotez@gmail.com>
2022-04-25 22:29:11 +0200

    nova klasa koja salje request na user service

---
+++ api/src/requester.py
@@ -0,0 +1,17 @@
+import requests
+
+class SendRequest():
+    """Slanje Requesta na userservice"""
+    def userService(name: str, lastName: str, mail: str, phoneNumber: int, password: str):
+        
+        url = 'http://fastapi_userservice:8080/insert-user'
+        data = {
+            "UserName" : name,
+            "UserLastName" : lastName,
+            "UserEmail" : mail,
+            "UserNumber" : phoneNumber,
+            "UserPassword" : password
+            }
+        requests.post(url, json = data)
+
+        return {"POST request send on": url}    # url se brise ovo je za test fazu, lakse dizanje excep-a, debug i loggove
