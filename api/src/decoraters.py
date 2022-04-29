64b7bb3c7c39ded9c281d6159c3674e5420aadbf
Mifa43 <mifa43kotez@gmail.com>
2022-04-29 21:16:27 +0200

    dodat je re lib koja proverava validnost email adrese i kreiran novi dekorater

--- api/src/decoraters.py
+++ api/src/decoraters.py
@@ -1,3 +1,4 @@
+import re
 
 class InputMustBeLower(object):
     # kreiranje dekoratera koji ce vratiti input malim slovima
@@ -7,3 +8,23 @@ class InputMustBeLower(object):
 	def __call__(self, name: str, email: str):
 		lower = self._arg(name.lower(), email.lower())
 		return lower
+
+class InputEmailIsEmail(object):
+	# kreiranje dekoratera sa re lib koji proverava da li je input email validan 
+	def __init__(self, regex):
+		"""
+		### - regex lista simbola
+		"""
+		self.regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+	def __call__(self, email: str):
+		"""na poziv dekoratera __call__ se izvrsava"""
+
+		if (re.fullmatch(self.regex, email)):
+			return {"EmailIsValid": True, "Email": email}
+
+		else: 
+			return {"EmailIsValid": False, "Email": None}
+
+		
+
