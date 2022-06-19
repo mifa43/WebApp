ac2eec7a7c8bb651029f7dc8451d3417342bb49d
Mifa43 <mifa43kotez@gmail.com>
2022-06-19 11:22:33 +0200

    izbacen visak importa i updejtovan kod za upisivanje u bazu

--- userservice/src/crud.py
+++ userservice/src/crud.py
@@ -1,22 +1,11 @@
-from click import echo
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
-from sqlalchemy import create_engine, Integer, String, ForeignKey, Column
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 from tableModel import User
-import os
 
 class Postgres():
     def __init__(self):
-        self.engine = create_engine(
-                    f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}",
-                    isolation_level="SERIALIZABLE", echo=True)
-        self.Base = declarative_base()
-        self.Base = User()
-        self.Base.metadata.create_all(bind=self.engine)
+        pass
         
-    def insert(self,userName: str, name: str, lastName: str, mail: str, phoneNumber: str, password: str) -> dict:
+    def insert(self,userName: str, name: str, lastName: str, mail: str, phoneNumber: str, password: str, db) -> dict:
         """ ## Insert
             ## Upisivanje korisnika u bazu
             - ``name``
@@ -24,30 +13,26 @@ class Postgres():
             - ``mail``
             - ``phoneNumber``
             - ``password``
+            - ``db``
+                - db == FastAPI(Session=Depends(get_db))
+                ##### osluskuje i otvara seassi-u
 
             ### Koristi User klasu, poziva atribute i dodeljuje vrednosti.
         """
         try:
-            Seassion = sessionmaker(bind=self.engine)
-            seassion = Seassion()
+            data = User(
+                name=userName,
+                firstName=name,
+                lastName=lastName,
+                mail=mail,
+                phoneNumber=phoneNumber,
+                password=password
+            )
+            db.add(data)
+            db.commit()
 
-            #Dodeljivanje vrednosti
-            user = User()
-            user.name = userName
-            user.firstName = name
-            user.lastName = lastName
-            user.mail = mail
-            user.phoneNumber = phoneNumber
-            user.password = password
-
-            #Izvrsavanje 
-            seassion.add(user)
-            seassion.commit()
-
-            #Zatvaranje seassi-e
-            seassion.close()
         except SQLAlchemyError as e:
             #hvata exception ako pokusa da se upise korisnik sa istim emailom jer je polje unique
             return {"postgresError": e, "error": True}
 
-        return {"UserInserted": name, "tableName": User.__tablename__, "error": False}
+        return {"UserInserted": name, "tableName": data.__tablename__, "error": False}
