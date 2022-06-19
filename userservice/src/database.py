d5b3c536cad3fc8b74679028478813e2a5a989c4
Mifa43 <mifa43kotez@gmail.com>
2022-06-19 11:21:57 +0200

    kreiranje sessi-e koja osluskuje prilikom pozivanja

---
+++ userservice/src/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+SQLALCHEMY_DATABASE_URL=f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    """Otvaramo seassi-u ka bazi"""
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
