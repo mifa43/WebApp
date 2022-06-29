66bc71e22a284b93d21432381257688cabc7949c
Mifa43 <mifa43kotez@gmail.com>
2022-06-29 21:27:32 +0200

    kreirana je nova klasa koja ce da salje email verifikaciju, prva metoda uzima keycloak user id koji je potreban za slanje email verifikacije

---
+++ api/src/resend_email_verfy.py
@@ -0,0 +1,45 @@
+from keycloak import KeycloakOpenID
+from keycloak import KeycloakAdmin
+import os
+
+class ResendVerifyEmail():
+    
+    def __init__(self):
+        # Kreiranje konekcije ka keycloak servisu
+
+        self.admin = keycloak_admin = KeycloakAdmin(
+                server_url="http://keycloak_keycloak_1:8080/auth/",
+                username = "admin",
+                password = "Pa55w0rd",
+                realm_name = "master",
+                verify = True)
+
+        # Konektovanje na realm kao klijent
+
+        self.openID = openID = KeycloakOpenID(
+                server_url = "http://keycloak_keycloak_1:8080/auth/",
+                client_id = "appuser",
+                realm_name = "demo",
+                client_secret_key = "f550d47f-a4cd-41f3-ab0d-d72936b634af"
+                )
+
+    def getKeycloakUserID(self, username):
+        """
+        Uzmi Keycloak user id.
+        Parametri:
+        ---------------
+            user_id_keycloak -> username : None -> bool(False) - ako nema username
+            user_id_keycloak -> username : bool(True), user_id - ima username
+        """
+
+        self.admin.realm_name = os.getenv('CLIENT_RELM_NAME')
+
+        user_id_keycloak = self.admin.get_user_id(self.username)
+
+        if user_id_keycloak == None:
+
+            return {"exist" : False}
+
+        else:
+
+            return {"exist" : True, "user_id_keycloak" : user_id_keycloak}
