65a586a2b84d00583ee5d7b28e31f0faf44f014d
Mifa43 <mifa43kotez@gmail.com>
2022-07-02 11:52:35 +0200

    dodat basic setup sa keycloakom

---
+++ auth/src/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, HTTPException, BackgroundTasks
+import logging, uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+# kreiranje logera https://docs.python.org/3/library/logging.html
+logger = logging.getLogger(__name__) 
+logger.setLevel("DEBUG")
+
+# kreiranje logger consol
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# format
+formatter = logging.Formatter('%(levelname)s:     %(name)s.%(funcName)s: %(message)s')
+
+# dodaj format u consol-u
+ch.setFormatter(formatter)
+
+# dodaj consol-u logger
+logger.addHandler(ch)
+app = FastAPI()
+
+origins = [
+    "*",
+    "http://0.0.0.0:8081/"
+]
+# allow cors request
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*","post"],
+    allow_headers=["*"],
+)
+# https://fastapi.tiangolo.com/tutorial/cors/
+
+@app.get("/")
+async def helth_check():
+    logger.info("{Health : OK}, 200")
+
+   
+    return {"Health": "OK"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, loop="asyncio")
