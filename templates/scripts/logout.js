2e083ac3f650fa7a51b975f3b305483e3495299d
Mifa43 <mifa43kotez@gmail.com>
2022-07-03 09:23:44 +0200

    kreirana logout skripta koja salje refresh token na auth api i brise token iz local storage-a

--- templates/scripts/logout.js
+++ templates/scripts/logout.js
@@ -1,6 +1,33 @@
 
-function logout(){
-    window.localStorage.removeItem("access_token");
-    console.log("You just logged out");
-    window.location="index.html";
+async function logout(){
+    // uzmi token iz local storage
+    let refreshToken = {
+        token: window.localStorage.getItem("access_token")
+        
+    }
+    // posalji refresh token na auth a;pi
+    try {
+        const response = await axios.post("http://0.0.0.0:8083/logout", refreshToken);
+    
+
+        // console.log(response.data.access_token.refresh_token);
+        // izbrisi token iz local storage
+        window.localStorage.removeItem("access_token");
+        
+        console.log("You just logged out");
+        
+        return window.location.href="index.html";
+
+    } catch (error) {
+        if (error.response) {
+            
+            console.log(error.message);
+            
+            
+        } else {
+            console.log(error.message);
+           
+          
+        }
+    }
 }
