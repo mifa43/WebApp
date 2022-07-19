326cdf6765ab674594b6cb5990a0efb980872764
Mifa43 <mifa43kotez@gmail.com>
2022-07-19 21:39:11 +0200

    test

---
+++ templates/scripts/restartPasswordPopup.js
@@ -0,0 +1,34 @@
+
+function putElem(){
+    document.getElementById('pop123').innerHTML = '\
+    <div class="subscribe-form">\
+        <div class="left">\
+            <img src="css/images/restart-password-img.jpg" alt="Forgot your password ilustration image " width="100%">\
+        </div>\
+        <div class="right">\
+            <h2>let us help you!</h2>\
+            <p>If you have forgotten your account password, you need to provide us with the email address you used during registration.</p>\
+            <div class="formm">\
+                <div class="inputt">\
+                    <label for="email">Your Email</label>\
+                    <input type="email" id="email" placeholder="alex@gmail.com">\
+                </div>\
+                <div class="button">\
+                    <button>Subscribe</button>\
+                </div>\
+            </div>\
+        </div>\
+        <button class="close-btn">\
+            &times;\
+        </button>\
+    </div>\
+    ';
+
+    document.querySelector("#open-subscribe-form").addEventListener("click",function(){
+        document.querySelector(".subscribe-form").classList.add("active");
+    });
+    document.querySelector(".subscribe-form .close-btn").addEventListener("click",function(){
+        document.querySelector(".subscribe-form").classList.remove("active");
+    });
+    return 0;
+}
