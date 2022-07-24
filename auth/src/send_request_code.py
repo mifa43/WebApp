ff4e8cde2f007f807a261f89c1ad3279462eab72
Mifa43 <mifa43kotez@gmail.com>
2022-07-24 18:59:34 +0200

    dodti komentari

--- auth/src/send_request_code.py
+++ auth/src/send_request_code.py
@@ -1,21 +1,35 @@
+from http.client import responses
 import requests
 import os
 
 class CreateCodeUserService():
-
+    """ ### slanje requesta na userservice za restartovanje passworda
+    __________________________________
+    - `email` -> registrovani email *samo ako postoji
+    - `code` -> randomCodeGen *upisuje se u db radi kasnije provere
+    
+    """
     def __init__(self, email, code):
 
         self.email = email
         self.code = code
 
-    def sendCode(self):
+    def sendCode(self) -> str:
+        """ ### Posalji request 
+        _________
+        - `url` -> docker service url *HOST:PORT
+        - `data` -> json body param *EMAIL:CODE
+        """
+        # docker url
         url = f'http://{os.getenv("USERSERVICE_HOST")}:{os.getenv("USERSERVICE_PORT")}/password-restart'
 
+        # data body
         data = {
             "UserEmail" : self.email,
             "code" : self.code
             }
 
+        # r Response
         r = requests.post(url, json = data)
 
         return {"PostRequestSendOn": url, "code": self.code, "response": r.json()}
