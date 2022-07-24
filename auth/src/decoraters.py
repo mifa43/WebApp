f732c268e050428011774e4cc0f1422ecaedbd12
Mifa43 <mifa43kotez@gmail.com>
2022-07-24 18:59:29 +0200

    dodati komentari i izbaceni neki delovi koda

--- auth/src/decoraters.py
+++ auth/src/decoraters.py
@@ -2,8 +2,7 @@ import re, random
 import asyncio
 
 class PasswordIsEqualToPassword(object):
-    def __init__(self, arg):
-        """### dekorater za proveru passworda
+    """### dekorater za proveru passworda
         
             - : `password1`
             - : `password2`
@@ -11,18 +10,24 @@ class PasswordIsEqualToPassword(object):
             #### ako je pass1 jedank pass2 vracamo `passwordIsValid`: `True`
 
         """
+    def __init__(self, arg):
+        
         self._arg = arg
 
     def __call__(self, password1: str, password2: str):
-        if password1 is not None and password2 is not None:
 
-            if password1 == password2:
+        if password1 is not None and password2 is not None: # ako je pass1 i pass2 None nije dosao password
+            # note: za restartovanje lozinke email je obavezan ali ako se korisnik seti lozinke ili iz nekog razloga odluci da ne iskoristi randomGenCode \
+            # nema potrebe da se proverava i menja lozinka prsto se proces obustavlja
+
+            if password1 == password2:  # pass1 i pass2 se podudaraju !
                 
                 return {"check": password1, "passwordIsValid": True, "passwordMustBeSent": True}
 
-            else:
+            else:   # lozinke nisu iste
                 
                 return {"check": [password1, password2], "passwordIsValid": False}
 
-        else:
+        else:   # cekaj password
+
             return {"check": "wait for password", "passwordMustBeSent": False}
