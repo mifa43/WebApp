d079b2f460d92cf132cd5080077dd62bc984bef2
Mifa43 <mifa43kotez@gmail.com>
2022-07-24 16:35:54 +0200

    dodati novi modeli email je obavezno polje jer korisnik prvo mora da dokaze da je vlasnik naloga zato su code i pass opciona polja

--- auth/src/models.py
+++ auth/src/models.py
@@ -10,5 +10,15 @@ class AuthCreaditional(BaseModel):
 class RefreshToken(BaseModel):
     token: str
 
+
+
 class UserPasswordRestart(BaseModel):
     UserEmail: str
+    code: Optional[str] = None
+    
+
+class RestartPassword(UserPasswordRestart):
+    password1: Optional[str] = None
+    password2: Optional[str] = None
+
+
