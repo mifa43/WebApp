523d0ddb2fac7cdfb8324a15aa5c03f323695e55
Mifa43 <mifa43kotez@gmail.com>
2022-07-24 16:40:19 +0200

    dodat id i izmenjen tekst

--- templates/scripts/restartPasswordPopup.js
+++ templates/scripts/restartPasswordPopup.js
@@ -9,16 +9,16 @@ function putElem(){
             <h2>let us help you!</h2>\
             <p>If you have forgotten your account password, you need to provide us with the email address you used during registration.</p>\
             <div class="formm">\
-                <div class="inputt">\
+                <div id="lab1" class="inputt">\
                     <label for="email">Your Email</label>\
                     <input type="email" id="email" placeholder="alex@gmail.com">\
                 </div>\
-                <div class="button">\
-                    <button>Subscribe</button>\
+                <div id="lab2" class="button">\
+                    <button id="restartPassword" onclick="restartPassword();" >Send code on email</button>\
                 </div>\
             </div>\
         </div>\
-        <button class="close-btn">\
+        <button  class="close-btn">\
             &times;\
         </button>\
     </div>\
