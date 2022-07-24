3c2478057b39be35e07e5c11469c49e8b7d7310f
Mifa43 <mifa43kotez@gmail.com>
2022-07-24 18:58:52 +0200

    dodati komentari objasnjena

--- auth/src/codeGen.py
+++ auth/src/codeGen.py
@@ -1,10 +1,17 @@
 import random
 import string
 
-def recoveryCodeGenerator():
-    # With combination of lower and upper case
+def recoveryCodeGenerator() -> str:
+    """ ### Kreiranje generatora kodova za restartovanje passworda
+        __________________
+        - return `code`
+    
+    """
+    # kombinacija asci brojeva i slova, velikih i malih
+
     characters = string.ascii_letters + string.digits 
+
+    #kod je duzine 8 karaktera i nasumicno generisan
     code = ''.join(random.choice(characters) for i in range(8))
 
-    # print random string
     return code
