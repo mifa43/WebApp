eedb7d0cc2f38a5e09ecc0b0e33abf4b44194840
Mifa43 <mifa43kotez@gmail.com>
2022-07-24 16:33:59 +0200

    pomocna funkcija koja ima class dekorater

---
+++ auth/src/helpers.py
@@ -0,0 +1,14 @@
+import re, asyncio
+from decoraters import *
+
+@PasswordIsEqualToPassword
+def checkPassword(password1, password2):
+    """### Pomocna funkcija
+
+       #### Pre izvrsavanja funkcije dekorater uzima argumente, \
+        proverava da li su pass1 i pass2 isti i vraca u funkciju
+    
+        - :`password1`
+        - :`password2`
+    """
+    return password1
