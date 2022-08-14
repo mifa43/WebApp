c9b7fb2006d58738eb835e9636410c4ab1edf23a
Mifa43 <mifa43kotez@gmail.com>
2022-08-14 11:20:15 +0200

    kreiranje corutina za keycloakLogin

--- auth/src/keycloakManager/keycloakLogin.py
+++ auth/src/keycloakManager/keycloakLogin.py
@@ -1,5 +1,5 @@
 from keycloakManager.keycloakConnection import Connection
-
+import asyncio
 class Login(Connection):
     """### Login (saljemo user creaditionale i dobijamo token)
             - `email`
@@ -14,13 +14,16 @@ class Login(Connection):
 
         super().__init__()  # nasledjivanje parent klase
 
-    def getToken(self):
+    async def getToken(self):
         
         """### Login (saljemo user creaditionale i dobijamo token)
             - `email`
             - `secret`
         """
         # Ulazimo u realm demo kao admin
+        await asyncio.sleep(0)
         token = self.openID.token(self.email, self.secret)
 
         return token
+
+# login 0.5 logout 0.2
