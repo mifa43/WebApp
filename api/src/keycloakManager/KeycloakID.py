23ea1274acd1dda967500ce57eef7e7230a7d960
Mifa43 <mifa43kotez@gmail.com>
2022-08-17 07:48:04 +0200

    rekonstruisane klase i pozvane u main

---
+++ api/src/keycloakManager/KeycloakID.py
@@ -0,0 +1,34 @@
+from keycloakManager.keycloakConnection import Connection
+import asyncio
+
+
+class GetKeycloakID(Connection):
+
+    def __init__(self, username) -> None:
+
+        self.username = username
+
+        super().__init__()
+
+    async def userID(self):
+        """
+        ### Uzmi Keycloak user id.
+        
+        ---------------
+            - `user_id_keycloak` -> `username` : None -> bool(False) - ako nema username
+            - `user_id_keycloak` -> `username` : bool(True), user_id - ima username
+        """
+
+        self.admin.realm_name = "demo"
+
+        user_id_keycloak = self.admin.get_user_id(self.username)
+
+        await asyncio.sleep(0)
+        
+        if user_id_keycloak == None:
+
+            return {"exist" : False}
+
+        else:
+
+            return {"exist" : True, "user_id_keycloak" : user_id_keycloak}
---
+++ api/src/keycloakManager/keycloakRegister.py
@@ -0,0 +1,51 @@
+from keycloakManager.keycloakConnection import Connection
+import asyncio
+
+class CreateUser(Connection):
+
+    def __init__(self, email: str, username: str, firstName: str, lastName: str, secret: str) -> None:
+        
+        self.email = email
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+        self.secret = secret
+
+        super().__init__()
+
+    async def new(self):
+        """### Kreiranje korisnika na Keycloak
+            - `email`
+            - `username`
+            - `firstName`
+            - `lastName` 
+            - `secret`
+        """
+              
+        # Ulazimo u realm demo kao admin
+ 
+        try:
+
+            data = self.admin.create_user({
+                "email": self.email,
+                "username": self.username,
+                "enabled": "True",
+                "firstName": self.firstName,
+                "lastName": self.lastName,
+                "credentials": [
+                    {
+                        "value": self.secret,
+                        "type": "password"
+                    }
+                ]
+                })
+
+            await asyncio.sleep(0)
+
+            # sendEmail = await self.admin.send_verify_email(user_id=data)
+           
+            return {"clientID": data, "userName": self.username, "kcError":  False,"emailStatus": True, "ID": data}
+
+        except:
+
+            return {"clientID": None, "userName": None, "kcError":  True, "ID": False}
