74d02bfa8468a73d709fb4ea78db80e23348a372
Mifa43 <mifa43kotez@gmail.com>
2022-08-24 07:48:03 +0200

    exception class

---
+++ auth/src/costumException/fastapiExceptions/fastapiExcepts.py
@@ -0,0 +1,18 @@
+
+
+
+class ValueInputException(Exception):
+
+    def __init__(self, param: str, msg: str) -> None:
+
+        self.param = param
+
+        self.msg = msg
+
+        super().__init__(msg)
+
+    def __str__(self) -> str:
+
+        return f"The entered value  is not valid and cannot be processed typeOFF=={type(self.param)}:length({len(self.param)}), msg={self.msg}"
+
+
