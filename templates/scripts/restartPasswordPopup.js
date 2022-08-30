ff33011abb9c19ffc01bef5685d048ca24a80068
Mifa43 <mifa43kotez@gmail.com>
2022-08-30 07:43:49 +0200

    ispravljen bug u jsu

--- templates/scripts/restartPasswordPopup.js
+++ templates/scripts/restartPasswordPopup.js
@@ -24,9 +24,8 @@ function putElem(){
     </div>\
     ';
 
-    document.querySelector("#open-subscribe-form").addEventListener("click",function(){
-        document.querySelector(".subscribe-form").classList.add("active");
-    });
+    document.querySelector(".subscribe-form").classList.add("active");
+
     document.querySelector(".subscribe-form .close-btn").addEventListener("click",function(){
         document.querySelector(".subscribe-form").classList.remove("active");
     });
