a0a71cd475b0d94160152b2c8b88202ba5db3b31
Mifa43 <mifa43kotez@gmail.com>
2022-09-11 13:07:44 +0200

    dodata ikonica za user profile pokazuje se samo ako je user pijavljen

--- templates/scripts/checkForToken.js
+++ templates/scripts/checkForToken.js
@@ -1,18 +1,19 @@
 function checkForToken(){
-    var token = window.localStorage.getItem("access_token");
+    var token = window.sessionStorage.getItem("access_token");
     console.log(token)
     // da li token postoji local storage ? d
     if(token === null){
         // ne ! prikazi login 
-        document.getElementById('authreq').innerHTML = '<li><a href="#"><i class="fa bi bi-box-arrow-in-right fa-2x"></i><span class="nav-text">Login</span></a></li></ul>';
+        document.getElementById('authreq').innerHTML = '<li><a href="#"><i class="fa bi bi-box-arrow-in-right fa-2x"></i><span class="nav-text">Login</span></a></li>';
 
         console.log("Token not found")
     }
     else {
-        // ne ! prikazi logout sa scriptom za logout
+        // ne ! prikazi logout sa scriptom za logout      
         console.log("Token founded")
+        document.getElementById('userProfile').innerHTML = '<a href="userProfile.html" ><i class="bi bi-person-workspace fa-2x userProfile"></i><span class="nav-text userProfileText">Profile</span></a>';
 
-        document.getElementById('authreq').innerHTML = '<li><a href="#" onclick="logout();"><i class="fa bi bi-power fa-2x"></i><span class="nav-text">Logout</span></a></li></ul>';
+        document.getElementById('authreq').innerHTML = '<li><a href="#" onclick="logout();"><i class="fa bi bi-power fa-2x"></i><span class="nav-text">Logout</span></a></li>';
 
     }
 }
