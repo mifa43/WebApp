89cdd8c9ff25dfe19ab08989275e3523dcb085b3
Mifa43 <mifa43kotez@gmail.com>
2022-10-08 12:02:36 +0200

    kreirana nova klasa koja salje request na api kao parametar uzima keycloakID

---
+++ userProfile/src/requester.py
@@ -0,0 +1,24 @@
+import  requests,os, asyncio
+import requests_async as requests
+
+class SendRequest():
+
+    def userService(keycloakUserID: str, session):
+        """Slanje Requesta na userservice"""
+      
+
+        #docker container ce biti u env variabli
+        # await asyncio.sleep(0)
+        url = f'http://{os.getenv("USERSERVICE_HOST")}:{os.getenv("USERSERVICE_PORT")}/get-user'
+
+        # request body
+
+        data = {
+            "keycloakUserID" : keycloakUserID
+            }
+
+        tasks = [] # lista
+
+        tasks.append(asyncio.create_task(session.get(url, params= data)))   # kreiramo corutine i dodajemo u listu
+
+        return {"PostRequestSendOn": url, "Response": tasks}
