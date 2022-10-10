b99d6262f36cce2ce51f381ba3fa850da4e06dcf
Mifa43 <mifa43kotez@gmail.com>
2022-10-10 11:32:31 +0200

    sada se u js salje formData tj slika i kao header se salje token

--- templates/scripts/profileImage.js
+++ templates/scripts/profileImage.js
@@ -1,9 +1,18 @@
 async function uploadFile() {
+    // polje koje uzima key: value i omogucava laksi rad multipart/form-data
     let formData = new FormData(); 
+
+    // dodajemo uploudovani fajl
     formData.append("file", fileupload.files[0]);
-    fetch('http://0.0.0.0:8085/user-profile-image', {
-      method: "POST", 
-      body: formData
-    }); 
-    alert('The file has been uploaded successfully.');
-    }
+
+    // slanje requesta, formData saljemo enkodovanu sliku = bytes, token saljemo kao header 
+    await axios.post('http://0.0.0.0:8085/user-profile-image/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'token': window.sessionStorage.getItem("access_token")
+      }
+    })
+}
+
+
+    
