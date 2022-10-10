e6a2db868c9d40687d7308797dd8e4f611cfdb46
Mifa43 <mifa43kotez@gmail.com>
2022-10-10 11:35:07 +0200

    dodata je nova metoda za slanje requesta

--- userProfile/src/requester.py
+++ userProfile/src/requester.py
@@ -22,3 +22,24 @@ class SendRequest():
         tasks.append(asyncio.create_task(session.get(url, params= data)))   # kreiramo corutine i dodajemo u listu
 
         return {"PostRequestSendOn": url, "Response": tasks}
+
+    def userServiceUpdate(imageUrl: str, keycloakUserID: str, session):
+            """Slanje Requesta na userservice"""
+        
+
+            #docker container ce biti u env variabli
+            # await asyncio.sleep(0)
+            url = f'http://{os.getenv("USERSERVICE_HOST")}:{os.getenv("USERSERVICE_PORT")}/update-user-image'
+
+            # request body
+
+            data = {
+                "imageUrl" : imageUrl,
+                "keycloakUserID" : keycloakUserID
+                }
+
+            tasks = [] # lista
+
+            tasks.append(asyncio.create_task(session.post(url, json = data)))   # kreiramo corutine i dodajemo u listu
+
+            return {"PostRequestSendOn": url, "Response": tasks}
