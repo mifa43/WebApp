43409a0b6d4ada6e80eeecb848b2281a3316cc68
Mifa43 <mifa43kotez@gmail.com>
2022-10-10 11:36:37 +0200

    test model

--- userProfile/src/models.py
+++ userProfile/src/models.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Dict, Optional
 
 class UserProfile(BaseModel):
     keycloakUserID: str
+
+class Token(BaseModel):
+    file: bytes = None
+    token: str
