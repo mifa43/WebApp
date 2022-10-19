464cba6bde0612f0ac7858ad06190d029c1c1f96
Mifa43 <mifa43kotez@gmail.com>
2022-10-19 14:10:49 +0200

    kreiran novi objekat za slanje requesta

--- userProfile/src/requester.py
+++ userProfile/src/requester.py
@@ -33,7 +33,7 @@ class SendRequest():
 
             #docker container ce biti u env variabli
             # await asyncio.sleep(0)
-            url = f'http://{os.getenv("USERSERVICE_HOST")}:{os.getenv("USERSERVICE_PORT")}/update-user-image'
+            url = f'http://{os.getenv("USERSERVICE_HOST")}:{os.getenv("USERSERVICE_PORT")}/update-user-profile'
 
             # request body
 
@@ -45,5 +45,30 @@ class SendRequest():
             tasks = [] # lista
 
             tasks.append(asyncio.create_task(session.post(url, json = data)))   # kreiramo corutine i dodajemo u listu
+
+            return {"PostRequestSendOn": url, "Response": tasks}
+
+    def userServiceUpdateProfile(firstName: str, lastName: str, mail: str, phoneNumber: str, userID: str, session):
+            """Slanje Requesta na userservice"""
+        
+
+            #docker container ce biti u env variabli
+            # await asyncio.sleep(0)
+            url = f'http://{os.getenv("USERSERVICE_HOST")}:{os.getenv("USERSERVICE_PORT")}/update-user-profile'
+                  
+
+            # request body
+
+            data = {
+                "UserFirstName": firstName,
+                "UserLastName": lastName,
+                "UserEmail": mail,
+                "UserNumber": phoneNumber,
+                "keycloakUserID": userID
+            }
+
+            tasks = [] # lista
+
+            tasks.append(asyncio.create_task(session.put(url, json = data)))   # kreiramo corutine i dodajemo u listu
 
             return {"PostRequestSendOn": url, "Response": tasks}
