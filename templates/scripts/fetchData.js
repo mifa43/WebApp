1dad9ae1ae12ec2115a735f26613dc5d9cb0e3cd
Mifa43 <mifa43kotez@gmail.com>
2022-10-21 17:25:51 +0200

    dodat je uslov ako je token null redirektujemo se na login

--- templates/scripts/fetchData.js
+++ templates/scripts/fetchData.js
@@ -4,30 +4,39 @@ async function fetchData(){
         token: window.sessionStorage.getItem("access_token")
       };
     // gadjamo profil servis
-    const response = await axios.get("http://0.0.0.0:8085/get-user-profile", {params} );
-
-    // uzimanjevrednosti iz response objekta
-    var firstName = response.data.data.firstName;
-    var phoneNumber = response.data.data.phoneNumber;
-    var mail = response.data.data.mail;
-    var lastName = response.data.data.lastName;
-    var keycloakUserID = response.data.data.keycloakUserID;
-    var userProfileIMG = response.data.data.imageURL;
-
-    // upisivanje vrednosti za vlasnika tokena
-    document.getElementById("userID").innerText = keycloakUserID;
-    document.getElementById("userName").innerText = firstName;
-    document.getElementById("userPhone").innerText = phoneNumber;
-    document.getElementById("userProf").innerText = lastName;    
-    document.getElementById("userEmail").innerText = mail;
-    if (userProfileIMG === null){
-      document.getElementById("userProfileIMG").src = "https://res.cloudinary.com/dt5xxftc5/image/upload/v1666083277/webApp/photo_q4cq9t.png";
-
+    if (params.token === null){
+      window.location = 'index.html';
     }
     else{
-      document.getElementById("userProfileIMG").src = userProfileIMG;
+      const response = await axios.get("http://0.0.0.0:8085/get-user-profile", {params} );
+
+      // uzimanjevrednosti iz response objekta
+      var firstName = response.data.data.firstName;
+      var phoneNumber = response.data.data.phoneNumber;
+      var mail = response.data.data.mail;
+      var lastName = response.data.data.lastName;
+      var keycloakUserID = response.data.data.keycloakUserID;
+      var userProfileIMG = response.data.data.imageURL;
+  
+      // upisivanje vrednosti za vlasnika tokena
+      document.getElementById("userID").innerText = keycloakUserID;
+      document.getElementById("userName").innerText = firstName;
+      document.getElementById("userPhone").innerText = phoneNumber;
+      document.getElementById("userProf").innerText = lastName;    
+      document.getElementById("userEmail").innerText = mail;
+      if (userProfileIMG === null){
+        document.getElementById("userProfileIMG").src = "https://res.cloudinary.com/dt5xxftc5/image/upload/v1666083277/webApp/photo_q4cq9t.png";
+  
+      }
+      else{
+        document.getElementById("userProfileIMG").src = userProfileIMG;
+  
+      }
 
     }
+
+
+
 
 
 
