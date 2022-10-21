0260f7795a1fbc2e2e7dd288a9adb8edb02f7747
Mifa43 <mifa43kotez@gmail.com>
2022-10-21 09:46:59 +0200

    doato polje deskripcije

--- templates/scripts/userProfileEditField.js
+++ templates/scripts/userProfileEditField.js
@@ -10,13 +10,15 @@ function editProfile(){
   let userEmail = document.getElementById("userEmail");
   let userPhone = document.getElementById("userPhone");
   let userProf = document.getElementById("userProf");
-  // dodat css style
+  let userDescription = document.getElementById("userDescription");
+
+  // dodat css style userDescription
   userID.classList.add("hide");
   userName.classList.add("hide");
   userEmail.classList.add("hide");
   userPhone.classList.add("hide");
   userProf.classList.add("hide");
-
+  userDescription.classList.add("hide");
   // dohvati tag izbrisi klasu hide
   
   // dohvati tag
@@ -25,12 +27,15 @@ function editProfile(){
   let userEmailInput = document.getElementById("userEmailInput");
   let userPhoneInput = document.getElementById("userPhoneInput");
   let userProfInput = document.getElementById("userProfInput");
+  let userDescriptionInput = document.getElementById("userDescriptionInput");
+
   // izbrisi klasu hide
   userIDInput.classList.remove("hide");
   userNameInput.classList.remove("hide");
   userEmailInput.classList.remove("hide");
   userPhoneInput.classList.remove("hide");
   userProfInput.classList.remove("hide");
+  userDescriptionInput.classList.remove("hide");
 
     // dodaj vrednost iz p taga
   userIDInput.value = userID.innerText;
@@ -38,12 +43,15 @@ function editProfile(){
   userEmailInput.value = userEmail.innerText;
   userPhoneInput.value = userPhone.innerText;
   userProfInput.value = userProf.innerText;
+  userDescriptionInput.value = userDescription.innerText;
 
   
 
 }
 
-
+//  querovanjem baze dobijamo user profile info, upisuje se u <p> tag, iz <p> taga uzimamo informazcije 
+//   i ubazcujemo ih u input, smena izmedju <p> i input taga zavisi od edit i save button-a, 
+//  save button salje request na nas api te se upisuje
 async function save(){
 
   document.getElementById("userID").classList.remove("hide");
@@ -51,6 +59,7 @@ async function save(){
   document.getElementById("userEmail").classList.remove("hide");
   document.getElementById("userPhone").classList.remove("hide");
   document.getElementById("userProf").classList.remove("hide");
+  document.getElementById("userDescription").classList.remove("hide");
 
 
   document.getElementById("userIDInput").classList.add("hide");
@@ -58,6 +67,7 @@ async function save(){
   document.getElementById("userEmailInput").classList.add("hide");
   document.getElementById("userPhoneInput").classList.add("hide");
   document.getElementById("userProfInput").classList.add("hide");
+  document.getElementById("userDescriptionInput").classList.add("hide");
 
 
   document.getElementById("saveBtn").classList.add("hide");
