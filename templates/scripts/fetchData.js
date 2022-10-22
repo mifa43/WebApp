b848a07ba8df5c2ce61c4526778870e5539be922
Mifa43 <mifa43kotez@gmail.com>
2022-10-22 14:05:16 +0200

    izmena u hvatanju respounse objekta

--- templates/scripts/fetchData.js
+++ templates/scripts/fetchData.js
@@ -11,12 +11,12 @@ async function fetchData(){
       const response = await axios.get("http://0.0.0.0:8085/get-user-profile", {params} );
 
       // uzimanjevrednosti iz response objekta
-      var firstName = response.data.data.firstName;
-      var phoneNumber = response.data.data.phoneNumber;
-      var mail = response.data.data.mail;
-      var lastName = response.data.data.lastName;
-      var keycloakUserID = response.data.data.keycloakUserID;
-      var userProfileIMG = response.data.data.imageURL;
+      var firstName = response.data.data.query.firstName;
+      var phoneNumber = response.data.data.query.phoneNumber;
+      var mail = response.data.data.query.mail;
+      var lastName = response.data.data.query.lastName;
+      var keycloakUserID = response.data.data.query.keycloakUserID;
+      var userProfileIMG = response.data.data.query.imageURL;
   
       // upisivanje vrednosti za vlasnika tokena
       document.getElementById("userID").innerText = keycloakUserID;
