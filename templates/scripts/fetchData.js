6e8a1deabe740755ba0b660b38ae49e02daaa06c
Mifa43 <mifa43kotez@gmail.com>
2022-11-06 15:28:33 +0100

    hvatanje respouns objekta i provera da li je prvi korak True

--- templates/scripts/fetchData.js
+++ templates/scripts/fetchData.js
@@ -17,6 +17,13 @@ async function fetchData(){
       var lastName = response.data.data.query.lastName;
       var keycloakUserID = response.data.data.query.keycloakUserID;
       var userProfileIMG = response.data.data.query.imageURL;
+      var description = response.data.data.query.description;
+      var title = response.data.data.query.title; 
+      var about = response.data.data.query.about;
+      var tags = response.data.data.query.tags; 
+      var location = response.data.data.query.location;
+      var links = response.data.data.query.links;
+      var firstStepsComplete = response.data.data.query.firstStepsComplete;
   
       // upisivanje vrednosti za vlasnika tokena
       document.getElementById("userID").innerText = keycloakUserID;
@@ -24,6 +31,8 @@ async function fetchData(){
       document.getElementById("userPhone").innerText = phoneNumber;
       document.getElementById("userProf").innerText = lastName;    
       document.getElementById("userEmail").innerText = mail;
+
+      if(firstStepsComplete==false){window.location = "setupUserProfile.html";};
       if (userProfileIMG === null){
         document.getElementById("userProfileIMG").src = "https://res.cloudinary.com/dt5xxftc5/image/upload/v1666083277/webApp/photo_q4cq9t.png";
   
@@ -33,12 +42,8 @@ async function fetchData(){
   
       }
 
+      
     }
-
-
-
-
-
 
     // window.location = 'userProfile.html';
 }
