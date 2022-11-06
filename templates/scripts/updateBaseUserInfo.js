cb68f24066775b9e739d0aa2f62cf924efca48bd
Mifa43 <mifa43kotez@gmail.com>
2022-11-06 19:11:26 +0100

    sanje requesta

--- templates/scripts/updateBaseUserInfo.js
+++ templates/scripts/updateBaseUserInfo.js
@@ -1,13 +1,26 @@
-function postData() {
-    var allinputs = document.querySelectorAll('.form-control');
-    var myLength = allinputs.length;
-    var input;
-  
-    for (var i = 0; i < myLength; ++i) {
-      input = allinputs[i];
-      if (input.value) {
-        window.alert(input.value);
-        }
-    }
+async function postData() {
+  var allinputs = document.querySelectorAll('.form-control');
+  var token = window.sessionStorage.getItem("access_token");
+  var myLength = allinputs.length;
+  var input;
+  var dict={"firstStepsComplete": true, "token": token};
+
+  for (var i = 0; i < myLength; ++i) {
+    input = allinputs[i];
+    if (input.value) {
+          dict[input.id] = input.value;
+
+      }
   }
-  
+  console.log(dict)
+  const resp = await fetch('http://0.0.0.0:8085/update-user-profile', {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: dict
+  });
+
+
+}
