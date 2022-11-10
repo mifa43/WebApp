97785e6b2409e1fff4949a64d28d8e38e59d4c4b
Mifa43 <mifa43kotez@gmail.com>
2022-11-10 09:52:33 +0100

    kada nam stigne json body , pravimp poroveru da li je neki key jedank None, Ako jeste to znaci da za to polje nije potreban update, tak key: value se izbacuje i updejta se samo gde su vrednosti razlicite od None

--- userservice/src/decoraters.py
+++ userservice/src/decoraters.py
@@ -17,5 +17,17 @@ class ModelToDict(object):
 			x = tuple(lis)  #elemente liste konvertujemo u tuple
 			
 			dic = self._arg(dict((key, value) for key, value in x)) #for rastavlja iz tuple-a key, vale i vraca kao dict
-			
-		return dic
+
+		l = {}	# prazan dict u koji se upisuju vrednosti koje su razlicite od None
+
+		for key in list(dic.keys()):	# forujemo dict koji je force konvertovan u listu kako bi izbegli gresku: RuntimeError: dictionary changed size during iteration
+
+			if dic[key] == None:	# ako je vrednost key-a jednaka None
+
+				del dic[key]	# brisem key koji je None
+
+			else:	# ako je bilo koja vrednost osim None, dodajemo je u novi dict kao kopiju i smatramo je validnom za update body
+		
+				l[key] = dic[key]	# kopiraj key i value u novi dict
+
+		return l
