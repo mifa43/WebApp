943863840dc6e3e5588de220c9484ed0a2831774
Mifa43 <mifa43kotez@gmail.com>
2022-11-10 09:52:47 +0100

    testiran helper funkcija

--- userservice/src/helpers.py
+++ userservice/src/helpers.py
@@ -15,7 +15,7 @@ def modelToDict(body):
     """
     return body
 
-# model = (('UserFirstName', 'mifa43'),('UserLastName', 'kotez23'),('UserEmail', 'mifa43kotez@gmail.com'),('UserNumber', '131313131313'),('keycloakUserID', '4253c73b-0fab-48e3-91d7-d3a7540605ef'))
+# model = (('UserFirstName', None),('UserLastName', None),('UserEmail', 'mifa43kotez@gmail.com'),('UserNumber', '131313131313'),('keycloakUserID', '4253c73b-0fab-48e3-91d7-d3a7540605ef'))
 
 # d = modelToDict(model)
 
