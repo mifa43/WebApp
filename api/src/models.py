9c0c1c2d230d08559f82ad79d67a04b28908fd4a
Mifa43 <mifa43kotez@gmail.com>
2022-11-13 13:28:31 +0100

    izbacen visak polja

--- api/src/models.py
+++ api/src/models.py
@@ -5,10 +5,7 @@ from pydantic import BaseModel
 # model za fastapi post metodu koja hvata requestove
 
 class RegisterForm(BaseModel):
-    UserName: str
-    UserLastName: str
     UserEmail: str
-    UserNumber: str
     UserPassword: str
     UserRePassword: str
     
