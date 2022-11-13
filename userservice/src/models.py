91c513e367cdf682183ba92d9d9b6f3d46ee28ba
Mifa43 <mifa43kotez@gmail.com>
2022-11-13 11:30:50 +0100

    za kreiranje korisnika, potrebno je samo keycloak id, user email

--- userservice/src/models.py
+++ userservice/src/models.py
@@ -5,11 +5,7 @@ from pydantic import BaseModel
 # model za hvatanje requesta i slanje u insert funkciju
 
 class UserModel(BaseModel):
-    UserName: str
-    UserFirstName: str
-    UserLastName: str
     UserEmail: str
-    UserNumber: str
     keycloakUserID: str
 
 class UpdateImage(BaseModel):
