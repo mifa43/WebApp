a1f68f96870e18c451bf764aab0771294cc6b900
Mifa43 <mifa43kotez@gmail.com>
2023-10-17 20:43:59 +0200

    izbacena je stara metoda gde se koristio json.dumps za slanje akcije

--- auth/src/keycloakManager/keycloakRestartPassword.py
+++ auth/src/keycloakManager/keycloakRestartPassword.py
@@ -18,10 +18,10 @@ class RestartPasswordKeycloak(Connection):
     async def user(self):
         """### Salje request za restartovanje passworda
         """
-        self.keycloak_admin.realm_name = "demo"
+        # self.keycloak_admin.realm_name = "demo"
         # self.admin.realm_name = "demo" # vazno je da se prebacimo na realm demo
         await asyncio.sleep(0)
-        response = self.admin.send_update_account(user_id=self.userID, payload=json.dumps(['UPDATE_PASSWORD'])) #{"user_id": userID, "payload": 'UPDATE_PASSWORD'}
-        
+        response = self.keycloak_admin.send_update_account(user_id=self.userID, payload=['UPDATE_PASSWORD']) #{"user_id": userID, "payload": 'UPDATE_PASSWORD'}
+
         # restart = self.admin.set_user_password(user_id=userID, password=userNewPassword, temporary=True)
         return response
